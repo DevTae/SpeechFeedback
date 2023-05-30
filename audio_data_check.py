7c8b38d075b1eb8fe62569ad916724af116816b8
Kim Taehyeon <dev.taehyeon.kim@gmail.com>
2023-05-31 01:42:30 +0900

    Create audio_data_check.py

---
+++ audio_data_check.py
@@ -0,0 +1,29 @@
+# Developed by DevTae@2023
+# For classifying and analyzing the audio datas, checking the data's duration per second is needed.
+
+import librosa
+
+path = "/workspace/kospeech/dataset/kspon/transcripts.txt"
+
+with open(path, "r", encoding="utf8") as r:
+  audio_paths = []
+  audio_durations = []
+  transcript_lengths = []
+  ratios = []
+  for line in r.readlines():
+    try:
+      audio_path, transcript, _ = line.split('\t');
+      audio_paths.append(audio_path)
+      audio_duration = librosa.get_duration(librosa.load(audio_path, sr=16000)[0], sr=16000)
+      audio_durations.append(audio_duration)
+      transcript_length = len(transcript)
+      transcript_lengths.append(transcript_length)
+      ratio = audio_duration / transcript_length
+      ratios.append(ratio)
+      print("ratio :", ratio)
+    catch:
+      continue
+      
+  print("max is", max(ratio))
+  print("min is", min(ratio))
+  print("avg is", sum(ratio)/len(ratio))
