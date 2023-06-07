15f6e5aebbfaf58884e585869e0e9aa035aeb4af
Kim Taehyeon <dev.taehyeon.kim@gmail.com>
2023-06-07 21:33:23 +0900

    Create unzip.sh
    - '한국어 음성' 데이터셋 압축 해제 스크립트 추가

---
+++ unzip.sh
@@ -0,0 +1,9 @@
+# Developed by DevTae@2023
+# This is the unzip script that extracts audio and transcript files
+# '한국어 음성' 폴더 안에 넣고 실행하면 각 디렉토리에 맞게 압축이 해제됩니다.
+unzip ./전시문_통합_스크립트/KsponSpeech_scripts.zip -d ./전시문_통합_스크립트/KsponSpeech_scripts
+unzip ./한국어_음성_분야/KsponSpeech_01.zip -d ./한국어_음성_분야
+unzip ./한국어_음성_분야/KsponSpeech_02.zip -d ./한국어_음성_분야
+unzip ./한국어_음성_분야/KsponSpeech_03.zip -d ./한국어_음성_분야
+unzip ./한국어_음성_분야/KsponSpeech_04.zip -d ./한국어_음성_분야
+unzip ./한국어_음성_분야/KsponSpeech_05.zip -d ./한국어_음성_분야
