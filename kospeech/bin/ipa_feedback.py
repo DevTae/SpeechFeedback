a7d12590de29dcb7bec4a6a86d7cbf9e6e63bfba
DevTae <dev.taehyeon.kim@gmail.com>
2023-06-13 00:11:47 +0900

    Update ipa_feedback.py to enhancement the algorithm of feedback

--- kospeech/bin/ipa_feedback.py
+++ kospeech/bin/ipa_feedback.py
@@ -16,11 +16,41 @@ def provide_feedback(standard_ipa, user_ipa):
     user_array = ft.word_array(features, user_ipa)
     
     feedback = []
-    for i, (s, u) in enumerate(zip(standard_array, user_array)):
-        for j, feature in enumerate(features):
-          if s[j] != u[j]:
-              if user_ipa_list[i] != standard_ipa_list[i]:
-                  feedback.append(f"{user_ipa_list[i]} 의 발음을 {feature} 를 사용해서 {standard_ipa_list[i]} 로 바꿔보세요.")
+    
+    # Developed by DevTae@2023
+    # 정답 labeling index 를 기준으로 삼고 한 개씩 오른쪽으로 포인터를 옮기면서 조건을 확인합니다.
+    # 입력 labeling index 에서 같은 문자가 나오지 않으면 바로 피드백합니다.
+    # 중복의 경우 유연하게 처리합니다.
+    i = 0
+    j = 0
+    find = False
+
+    while i < len(standard_ipa):
+        while i < len(standard_ipa) and (standard_ipa[i] == " " or standard_ipa[i] == "!" or standard_ipa[i] == "?" or standard_ipa[i] == "." or standard_ipa[i] == ","):
+            i += 1
+        if i < len(standard_ipa) and j < len(user_ipa) and standard_ipa[i] == user_ipa[j]:
+            find = True
+            j += 1
+            while j < len(user_ipa) and (user_ipa[j] == " " or user_ipa[j] == "!" or user_ipa[j] == "?" or user_ipa[j] == "." or user_ipa[j] == ","):
+                j += 1
+        else:
+            if find == True:
+                find = False
+                i += 1
+            
+                # 같은 문자가 나오는 동안에는 skip
+                while i < len(standard_ipa) and standard_ipa[i] == standard_ipa[i-1]:
+                    i += 1
+            else:
+                # feedback (standard[i] <-> user[j])
+                # standard_array[i] 를 활용하여 standard[i] 로 교정
+                arr = []
+                for index, feature in enumerate(features):
+                    if standard_array[i][index] != user_array[j][index]:
+                        arr.append(feature)
+                correction = ','.join(arr)
+                feedback.append(f"{user_ipa_list[j]} 의 발음을 {correction} 를 사용해서 {standard_ipa_list[i]} 로 바꿔보세요.")
+                break
     
     if not feedback:
         feedback.append("잘 하셨습니다.")
